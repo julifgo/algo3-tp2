671778049cc977e131853632d3d355f27d7ba642
agustinpenas <agustin.penas@hotmail.com>
2016-05-09 19:45:55 -0300

    Update retorno.h

--- src/retorno.h
+++ src/retorno.h
@@ -1,3 +1,4 @@
+
 #ifndef RETORNO_H_
 #define RETORNO_H_
 
@@ -6,6 +7,13 @@
 #include <iostream>
 #include <sstream>
 
-void ElRetornoDelJedi ();
+using namespace std;
+
+enum Direccion {arriba, abajo, izquierda, derecha, sinDireccion};
+
+
+void ElRetornoDelJedi(vector<vector<int> > grilla, int h, int n, int m);
+int max (int a, int b);
+int min (int a, int b);
 
 #endif /* RETORNO_H_ */
