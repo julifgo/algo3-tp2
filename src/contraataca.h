bae6123d844530280929e02cf6fbd0958ff2ee81
agustinpenas <agustin.penas@hotmail.com>
2016-05-09 19:10:26 -0300

    Update contraataca.h

--- src/contraataca.h
+++ src/contraataca.h
@@ -1,11 +1,36 @@
 #ifndef CONTRAATACA_H_
 #define CONTRAATACA_H_
 
-#include <stdio.h>
-#include <math.h>
 #include <iostream>
-#include <sstream>
+#include<cstdio>
+#include <vector>
+#include <limits>
 
-void ElImperioContraataca();
+using namespace std;
+
+
+
+struct Arista{
+	int nodoA;
+	int nodoB;
+	int peso;
+
+	Arista(int a, int b, int peso){
+		this->nodoA=a;
+		this->nodoB=b;
+		this->peso=peso;
+	}
+};
+
+vector<int> padre;
+vector<int> altura;
+
+void elImperioContraataca(int cn, vector< vector<int> > ady);
+void init(int n);
+int find(int x);
+void unir(int x, int y);
+void Merge(vector<Arista>& A,vector<Arista>& B, vector<Arista>& C);
+void MergeSort(vector<Arista>& A);
+
 
 #endif /* CONTRAATACA_H_ */
